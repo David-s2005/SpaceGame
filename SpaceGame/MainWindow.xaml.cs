be3b7c344335ac6c9597bf2c0d443b70de4d4564
David <David@DESKTOP-OFEBG0G>
2023-12-12 18:51:17 +0800

    Add project files.

---
+++ SpaceGame/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SpaceGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) 
+        {
+            LabelShield.Foreground = Brushes.LightGreen;
+            LabelHealth.Foreground = Brushes.LightGreen;
+            LabelAI.Foreground = Brushes.LightGreen;
+
+            LabelShield.Content = "100";
+            LabelHealth.Content = "100";
+            LabelAI.Content = "100";
+
+            imageExploration.Source = new BitmapImage(new Uri("C:/Users/David/projects/C#/SpaceGame/SpaceGame/Images/System/TEMP/placeholder.png", UriKind.RelativeOrAbsolute));
+            imageShip.Source = new BitmapImage(new Uri("C:/Users/David/projects/C#/SpaceGame/SpaceGame/Images/System/TEMP/placeholder.png", UriKind.RelativeOrAbsolute));
+            imageShipShield.Source = new BitmapImage(new Uri("C:/Users/David/projects/C#/SpaceGame/SpaceGame/Images/System/TEMP/placeholder.png", UriKind.RelativeOrAbsolute));
+            imageShipHealth.Source = new BitmapImage(new Uri("C:/Users/David/projects/C#/SpaceGame/SpaceGame/Images/System/TEMP/placeholder.png", UriKind.RelativeOrAbsolute));
+            imageShipAI.Source = new BitmapImage(new Uri("C:/Users/David/projects/C#/SpaceGame/SpaceGame/Images/System/TEMP/placeholder.png", UriKind.RelativeOrAbsolute));
+
+            imageIron.Source = new BitmapImage(new Uri("C:/Users/David/projects/C#/SpaceGame/SpaceGame/Images/System/Planets/Resources/iron.png", UriKind.RelativeOrAbsolute));
+            imageCopper.Source = new BitmapImage(new Uri("C:/Users/David/projects/C#/SpaceGame/SpaceGame/Images/System/Planets/Resources/copper.png", UriKind.RelativeOrAbsolute));
+            imagePlatinum.Source = new BitmapImage(new Uri("C:/Users/David/projects/C#/SpaceGame/SpaceGame/Images/System/Planets/Resources/platinum.png", UriKind.RelativeOrAbsolute));
+            imageUranium.Source = new BitmapImage(new Uri("C:/Users/David/projects/C#/SpaceGame/SpaceGame/Images/System/Planets/Resources/uranium.png", UriKind.RelativeOrAbsolute));
+            imageSilicon.Source = new BitmapImage(new Uri("C:/Users/David/projects/C#/SpaceGame/SpaceGame/Images/System/Planets/Resources/silicon.png", UriKind.RelativeOrAbsolute));
+        }
+    }
+}
---
+++ SpaceGame/ViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Media.Imaging;
+
+namespace SpaceGame
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private string _yourProperty;
+
+        public string YourProperty
+        {
+            get { return _yourProperty; }
+            set
+            {
+                if (_yourProperty != value)
+                {
+                    _yourProperty = value;
+                    OnPropertyChanged(nameof(YourProperty));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
